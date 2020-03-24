d8d9fd0583c89830aec2c5bf21302d1e57112a6a
吴瑞鹏 <137584321@qq.com>
2020-03-24 12:36:47 +0000

    first commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a >发声制作组 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right">联系我们 vx:137584321</p>
+  </div>
+</footer>
